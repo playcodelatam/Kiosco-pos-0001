23587f731527764c64131c91648e589b843ee640
playcodelatam <playcodeacademia@gmail.com>
2025-11-30 04:02:40 +0000

    Aumentar tamaño de elementos en Caja 30%
    - Aumentar fuentes de inputs y botones
    - Aumentar tamaño de iconos SVG (20-24px a 26-31px)
    - Aumentar padding y dimensiones de todos los elementos
    - Aumentar altura de sección importe (250px a 325px)
    - Mejorar legibilidad en dispositivos móviles
    
    Co-authored-by: Ona <no-reply@ona.com>

--- src/Components/Caja.jsx
+++ src/Components/Caja.jsx
@@ -346,9 +346,9 @@ const cobrar = async () => {
                 onClick={() => toggleMenu(item.codigo)}
               >
                 <svg xmlns="http://www.w3.org/2000/svg" 
-                  height="24px" 
+                  height="31px" 
                   viewBox="0 -960 960 960" 
-                  width="24px" 
+                  width="31px" 
                   fill="#000000">
                     <path d="M480-160q-33 0-56.5-23.5T400-240q0-33 23.5-56.5T480-320q33 0 56.5 23.5T560-240q0 33-23.5 56.5T480-160Zm0-240q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm0-240q-33 0-56.5-23.5T400-720q0-33 23.5-56.5T480-800q33 0 56.5 23.5T560-720q0 33-23.5 56.5T480-640Z"/>
                 </svg>
@@ -363,9 +363,9 @@ const cobrar = async () => {
                 } }
               >
                 <svg xmlns="http://www.w3.org/2000/svg" 
-                  height="20px" 
+                  height="26px" 
                   viewBox="0 -960 960 960" 
-                  width="20px" 
+                  width="26px" 
                   fill="white">
                     <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/>
                 </svg>
@@ -376,9 +376,9 @@ const cobrar = async () => {
                 onClick={() => { borrarDelCarrito(item.codigo)}}
               >
                 <svg xmlns="http://www.w3.org/2000/svg" 
-                  height="20px" 
+                  height="26px" 
                   viewBox="0 -960 960 960" 
-                  width="20px" 
+                  width="26px" 
                   fill="white">
                     <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
                 </button>
